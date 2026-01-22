5796f9af4e0bc0f287be306ee9dc4c9bebf08452
ishaku <ishakumarch@gmail.com>
2026-01-22 20:50:50 +0100

    i structured the base layer of the project by adding the fronend and the module layer

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ["../modules/base"],
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+});
